33194b3685cec7cee26bd786fd5f913c161657f3
globenewmedialtd <globenewmedialtd@gmail.com>
2021-07-21 20:07:21 +0200

    Timer fix

--- js/jquery_countdown_timer_init.js
+++ js/jquery_countdown_timer_init.js
@@ -5,28 +5,64 @@
      */
     Drupal.behaviors.jsCountdownTimer = {
         attach: function (context) {
-            var note = $('#jquery-countdown-timer-note'),
-                ts = new Date(drupalSettings.countdown.unixtimestamp * 1000);
 
-            $(context).find('#jquery-countdown-timer').once('jquery-countdown-timer').countdown({
-                timestamp: ts,
-                font_size: drupalSettings.countdown.fontsize,
-                callback: function (weeks, days, hours, minutes, seconds) {
-                    var dateStrings = new Array();
-                    dateStrings['@weeks'] = Drupal.formatPlural(weeks, '@count week', '@count weeks');
-                    dateStrings['@days'] = Drupal.formatPlural(days, '@count day', '@count days');
-                    dateStrings['@hours'] = Drupal.formatPlural(hours, '@count hour', '@count hours');
-                    dateStrings['@minutes'] = Drupal.formatPlural(minutes, '@count minute', '@count minutes');
-                    dateStrings['@seconds'] = Drupal.formatPlural(seconds, '@count second', '@count seconds');
-                    var message = Drupal.t('@hours : @minutes : @seconds', dateStrings);
-                    if (message === '0 hours : 0 minutes : 0 seconds') {
-                      $('#jquery-countdown-timer').hide();
-                      $('#virtual-event-bbb-link-form button').removeClass('visually-hidden');
-                    }
-	            //console.log(message);
-                    //note.html(message);
-                }
-            });
+            if(drupalSettings.countdown) {
+
+                $.each( drupalSettings.countdown, function( index, value ) {
+                    let ts = new Date(value.unixtimestamp * 1000);
+                    let form_class = value.form_class + ' button';
+                    
+                    $(context).find('#' + value.timer_element).once(value.timer_element).countdown({
+                        timestamp: ts,
+                        font_size: value.fontsize,
+                        callback: function (weeks, days, hours, minutes, seconds) {
+                            let dateStrings = new Array();
+                            dateStrings['@weeks'] = Drupal.formatPlural(weeks, '@count week', '@count weeks');
+                            dateStrings['@days'] = Drupal.formatPlural(days, '@count day', '@count days');
+                            dateStrings['@hours'] = Drupal.formatPlural(hours, '@count hour', '@count hours');
+                            dateStrings['@minutes'] = Drupal.formatPlural(minutes, '@count minute', '@count minutes');
+                            dateStrings['@seconds'] = Drupal.formatPlural(seconds, '@count second', '@count seconds');
+                            var message = Drupal.t('@hours : @minutes : @seconds', dateStrings);
+                            if (message === '0 hours : 0 minutes : 0 seconds') {
+                              $('#' + value.timer_element).hide();
+                              $('form.' + form_class).removeClass('visually-hidden');
+                            }
+                        }
+                    });
+
+                });
+                
+            }
+
+            if(drupalSettings.timer) {
+
+                $.each( drupalSettings.timer, function( index, value ) {
+                    let ts = new Date(value.unixtimestamp * 1000);
+                    let form_class = value.form_class + ' button';
+                    
+                    $(context).find('#' + value.timer_element).once(value.timer_element).countdown({
+                        timestamp: ts,
+                        font_size: value.fontsize,
+                        callback: function (weeks, days, hours, minutes, seconds) {
+                            let dateStrings = new Array();
+                            dateStrings['@weeks'] = Drupal.formatPlural(weeks, '@count week', '@count weeks');
+                            dateStrings['@days'] = Drupal.formatPlural(days, '@count day', '@count days');
+                            dateStrings['@hours'] = Drupal.formatPlural(hours, '@count hour', '@count hours');
+                            dateStrings['@minutes'] = Drupal.formatPlural(minutes, '@count minute', '@count minutes');
+                            dateStrings['@seconds'] = Drupal.formatPlural(seconds, '@count second', '@count seconds');
+                            let message = Drupal.t('@hours : @minutes : @seconds', dateStrings);
+                            if (message === '0 hours : 0 minutes : 0 seconds') {  
+                                $('form.' + form_class).addClass('visually-hidden');
+                                $('.block-social-virtual-event-bbb-join-button-block').addClass('visually-hidden');
+                            }
+
+                        }
+                    });
+
+                });
+                
+            }
+
         }
     };
 })(jQuery, Drupal, drupalSettings);
