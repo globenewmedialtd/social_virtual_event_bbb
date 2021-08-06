131fcd5e43060311de856bad1ac4e31f2ccb53f3
globenewmedialtd <globenewmedialtd@gmail.com>
2021-08-06 21:03:56 +0200

    Timer with headline.

--- js/jquery_countdown_timer_init.js
+++ js/jquery_countdown_timer_init.js
@@ -11,19 +11,24 @@
                 $.each( drupalSettings.countdown, function( index, value ) {
                     let ts = new Date(value.unixtimestamp * 1000);
                     let form_class = value.form_class + ' button';
+		    let note = $('#' + value.timer_element_note);
                     
                     $(context).find('#' + value.timer_element).once(value.timer_element).countdown({
                         timestamp: ts,
                         font_size: value.fontsize,
                         callback: function (weeks, days, hours, minutes, seconds) {
+                            let done = weeks + days + hours + minutes + seconds;
                             let dateStrings = new Array();
                             dateStrings['@weeks'] = Drupal.formatPlural(weeks, '@count week', '@count weeks');
                             dateStrings['@days'] = Drupal.formatPlural(days, '@count day', '@count days');
                             dateStrings['@hours'] = Drupal.formatPlural(hours, '@count hour', '@count hours');
                             dateStrings['@minutes'] = Drupal.formatPlural(minutes, '@count minute', '@count minutes');
                             dateStrings['@seconds'] = Drupal.formatPlural(seconds, '@count second', '@count seconds');
-                            var message = Drupal.t('@hours : @minutes : @seconds', dateStrings);
-                            if (message === '0 hours : 0 minutes : 0 seconds') {
+			    let message_headline = Drupal.t('Event starts in: ');
+			    let message = Drupal.t('@weeks, @days, @hours, @minutes, @seconds', dateStrings);
+                    	    note.html(message_headline + message);
+                            if (done == 0) {
+			      note.hide();
                               $('#' + value.timer_element).hide();
                               $('form.' + form_class).removeClass('visually-hidden');
                             }
